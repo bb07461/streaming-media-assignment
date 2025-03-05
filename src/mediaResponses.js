6f703cc5f33a234f74c4c2e5ea7a11c5f84e55e2
bb07461 <98509031+bb07461@users.noreply.github.com>
2025-03-05 10:53:59 -0500

    completed streaming
    working yay

---
+++ src/mediaResponses.js
@@ -0,0 +1,72 @@
+const fs = require('fs');
+const path = require('path');
+
+const streamFile = (request, response, filePath, contentType) => {
+  const file = path.resolve(__dirname, filePath);
+
+  fs.stat(file, (err, stats) => {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        response.writeHead(404);
+      }
+      console.error(err);
+      return response.end('Error loading file');
+    }
+
+    let { range } = request.headers;
+
+    if (!range) {
+      range = 'bytes=0-';
+    }
+
+    const positions = range.replace(/bytes=/, '').split('-');
+    let start = parseInt(positions[0], 10);
+
+    const total = stats.size;
+    const end = positions[1] ? parseInt(positions[1], 10) : total - 1;
+
+    if (start > end) {
+      start = 0; // Reset start to the beginning of the file
+    }
+
+    const chunksize = (end - start) + 1;
+
+    response.writeHead(206, {
+      'Content-Range': `bytes ${start}-${end}/${total}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': chunksize,
+      'Content-Type': contentType,
+    });
+
+    const stream = fs.createReadStream(file, { start, end });
+
+    stream.on('open', () => {
+      stream.pipe(response);
+    });
+
+    stream.on('error', (streamErr) => {
+      console.error(streamErr); // Log the error for debugging
+      response.end('Error streaming file');
+    });
+  });
+
+  return undefined;
+};
+
+const getParty = (request, response) => {
+  streamFile(request, response, '../client/party.mp4', 'video/mp4');
+};
+
+const getBling = (request, response) => {
+  streamFile(request, response, '../client/bling.mp3', 'audio/mpeg');
+};
+
+const getBird = (request, response) => {
+  streamFile(request, response, '../client/bird.mp4', 'video/mp4');
+};
+
+module.exports = {
+  getParty,
+  getBling,
+  getBird,
+};
